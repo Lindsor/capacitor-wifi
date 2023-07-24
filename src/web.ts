3dd0dcfd00663d1239d92e57aabcd160b2b58789
Marcelo Luz <marcelo.luz@lindsor.com>
2023-07-24 15:16:50 -0400

    Fix Lint Issues

--- src/web.ts
+++ src/web.ts
@@ -1,7 +1,6 @@
 import { WebPlugin } from '@capacitor/core';
 
 import type {
-  ConnectToWifiRequest,
   ConnectToWifiResult,
   GetCurrentWifiResult,
   ScanWifiResult,
@@ -18,9 +17,7 @@ export class WifiWeb extends WebPlugin implements WifiPlugin {
     throw this.unavailable(WifiErrorCode.METHOD_UNIMPLEMENTED);
   }
 
-  async connectToWifiBySsidAndPassword(
-    _request: ConnectToWifiRequest,
-  ): Promise<ConnectToWifiResult> {
+  async connectToWifiBySsidAndPassword(): Promise<ConnectToWifiResult> {
     throw this.unavailable(WifiErrorCode.METHOD_UNIMPLEMENTED);
   }
 }
